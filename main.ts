810564b03ed680b24683b0e7ab132d8cd3a9accd
Miaoge <8201072@gmail.com>
2025-03-11 11:11:19 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,8 @@
+// 智能高考志愿填报应用 - TypeScript入口文件
+// 这个文件作为Deno Deploy的入口点，导入并使用main.js中的功能
+
+// 导入main.js中的所有内容
+import "./main.js";
+
+// 这个文件只是作为TypeScript入口点，实际功能由main.js提供
+// 如果需要，可以在这里添加TypeScript特定的功能或类型定义
